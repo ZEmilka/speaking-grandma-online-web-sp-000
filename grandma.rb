e36b5412f105eae3afa5ed1fe85f7c0a9dc53b8c
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-07 04:25:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,8 @@ phrase="How are you Grandma?"
 def speak_to_grandma(phrase)
   if phrase==phrase.upcase
     return"NO, NOT SINCE 1938!"
-  elsif phrase=="I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  #elsif phrase=="I LOVE YOU GRANDMA!"
+    #return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
